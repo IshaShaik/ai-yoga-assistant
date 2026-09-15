98b80fd5db3cb841b3ec0ea56b971b27724a96aa
Isha Shaik <ishashaik1509@gmail.com>
2026-09-15 18:30:12 +0530

    Add PWA manifest and service worker for mobile install

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
